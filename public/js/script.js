509dc046689703ee19700973d57130ce56920ae4
kharadimihir <kharadimihir739@gmail.com>
2024-12-03 18:09:22 +0530

    Improve navbar and some icons

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,17 @@
       }, false)
     })
   })()
+
+
+  let taxSwitch = document.getElementById("flexSwitchCheckDefault");
+    taxSwitch.addEventListener("click", () => {
+        let taxInfo = document.getElementsByClassName("tax-info");
+        console.log(taxInfo);
+        for(let info of taxInfo){
+            if (info.style.display != "inline") {
+                info.style.display = "inline";
+            }else{
+                info.style.display = "none";
+            }
+        }
+    })
